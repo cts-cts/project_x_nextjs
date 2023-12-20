724c7a35db1df644157c91547acc6e7a35e4c667
SM Shohag <fsm.cts@gmail.com>
2023-12-20 18:02:06 +0600

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import VideoPlayerComponent from "./components/VideoPlayerComponent";
+import dynamic from "next/dynamic";
+// import "../app/globals.css";
+import "plyr-react/plyr.css";
+const VideoPlayerComponent = dynamic(
+  () => import("./components/VideoPlayerComponent"),
+  { ssr: false }
+);
 
 const HomePage = async () => {
   return (
